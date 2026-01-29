2088f770af0546bfdb13bf0d7887b2cf24c0d38a
GoGradually <chkun3109@gmail.com>
2026-01-30 02:11:43 +0900

    feat: AppModule에 PrismaModule, UserModule, PostModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [PrismaModule, UserModule, PostModule],  // 각종 모듈을 import
 })
 export class AppModule {}
